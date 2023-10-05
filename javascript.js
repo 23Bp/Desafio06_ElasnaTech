75aaf0cf04ee00cf55b8156164734a9b92836553
biancapillon2 <biancapillon2@gmail.com>
2023-10-05 14:04:39 -0300

    desafio6_aula11

---
+++ javascript.js
@@ -0,0 +1,17 @@
+alert('Hello World!');
+
+var nome = parseInt(prompt('Digite seu nome'));
+alert('Seja bem vinda!');
+
+var numero = parseInt(prompt('Digite um número inteiro'));
+
+    if (numero % 2 === 0 && numero > 0 && numero < 100) {
+        alert("O numero atende aos critérios");
+
+    }else{
+        alert("O numero não atende aos critérios");
+    }
+
+
+
+
